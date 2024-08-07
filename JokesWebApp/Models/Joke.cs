c2e54317324591288089e29990c344a2c5167869
Blaster <blasterbuild.161317@gmail.com>
2024-08-07 06:50:37 +0530

    completed

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string jokeQuetion { get; set; }
+        public string jokeAnswer { get; set; }
+
+        public Joke()
+        {
+                
+        }
+
+    }
+}
